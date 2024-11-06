0dad798d10bca4ee22eb3ce52de523eaa31aa583
Rednata <maknatosh@gmail.com>
2024-11-06 12:35:18 +0700

    modules, package.json

---
+++ 01-modules/index.js
@@ -0,0 +1,15 @@
+const dataUser = require('dataUser');
+
+const user = dataUser({
+  name: 'иван иванов',
+  dateBirth: '06.01.1987',
+  purpose: 'карьерный рост'
+})
+
+console.log('user: ', user);
+
+
+
+
+
+
